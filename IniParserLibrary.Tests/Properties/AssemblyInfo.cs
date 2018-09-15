9e271556f2d28295a6e378b18c722d51b7d34c0a
Daniil Shipilin <daniil.shipilin@gmail.com>
2018-09-15 10:20:59 +0300

    Added unit tests

---
+++ IniParserLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IniParserLibrary.Tests")]
+[assembly: AssemblyDescription("IniParserLibrary.Tests")]
+[assembly: AssemblyProduct("IniParserLibrary.Tests")]
+[assembly: AssemblyCopyright("Daniil Shipilin")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.2.0")]

02d4ed73927c010c85ca19f0d4d0172a556e611d
Daniil Shipilin <daniil.shipilin@gmail.com>
2020-10-19 11:40:28 +0200

    AssemblyInfo updated

--- IniParser/Properties/AssemblyInfo.cs
+++ IniParser/Properties/AssemblyInfo.cs
@@ -4,10 +4,4 @@ using System.Reflection;
 [assembly: AssemblyDescription("IniParserLibrary")]
 [assembly: AssemblyProduct("IniParserLibrary")]
 [assembly: AssemblyCopyright("Daniil Shipilin")]
-
-// Semantic Versioning Specification (https://semver.org/)
-// Given a version number MAJOR.MINOR.PATCH, increment the:
-// MAJOR version when you make incompatible API changes,
-// MINOR version when you add functionality in a backwards-compatible manner, and
-// PATCH version when you make backwards-compatible bug fixes.
-[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyVersion("0.7.0")]

1b6118ca10f7f6fc0dc04415731ef9901704bb00
Daniil Shipilin <daniil.shipilin@gmail.com>
2025-08-09 12:27:35 +0200

    Added case sensitivity unit test

--- IniParser.Tests/Tests.cs
+++ IniParser.Tests/Tests.cs
@@ -9,7 +9,7 @@ namespace IniParser.Tests;
 [TestFixture]
 public class Tests
 {
-    private const string EXAMPLEINISHA512 = "82327697e09473dbb9567749f127fd2e96ced5581b55ff413f6feabe49073701377f640b3cff4071b338b4e8aaa638e11d9bc86f54feda62c2c848245578cf62";
+    private const string EXAMPLEINISHA256 = "418ff6365eafacaeca0e0f7191ba8f8ab20158503833ba4a8f8c810bee1c1fdf";
 
     private const string SECTION1 = "Numbers";
     private const string SECTION2 = "Strings";
@@ -17,6 +17,8 @@ public class Tests
     private const string NONEXISTENTSECTION = "NonExistentSection";
     private const string SECTIONWITHNOKEYS = "SectionWithNoKeys";
     private const string SECTIONWITHEMPTYKEY = "SectionWithEmptyKey";
+    private const string CASESENSITIVESECTION1 = "CaseSensitiveSection";
+    private const string CASESENSITIVESECTION2 = "casesensitivesection";
 
     private const string KEY1 = "Binary";
     private const string KEY2 = "Octal";
@@ -28,6 +30,8 @@ public class Tests
     private const string KEY8 = "SHA1";
     private const string KEY9 = "Image";
     private const string KEY10 = "ImageSHA512";
+    private const string KEY11 = "Key";
+    private const string KEY12 = "key";
     private const string EMPTYKEY = "EmptyKey";
 
     private const string VALUE1 = "11001010";
@@ -40,6 +44,7 @@ public class Tests
     private const string VALUE8 = "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0";
     private const string VALUE9 = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuMjHxIGmVAAAElElEQVRYR9VXW0hsZRQe65xRx/ttdC6OM6OOHm8J3htwvJR2vIsoIoyKD2ooSqJoIoyjaB68RGSWVCg9BBH4EBGCWD2cRzkPig9BRASdc6DoBiXEjKv1/e1tG9NxztEGWvDx7/1f1lr/+tf6/r1V/wv58Y3iSHI4bhGpgqSuwAmpVEGe9wxO7yfWhT9X9M+Ty3FLGgqM0Gb/bc/7hlc83xY+8u4lf/5LlylGGgqc0GumAu9nOfe99xJWqT1LLXUHTn5bscWfulJKT3eGbO3t7c9yV4CP4VOtte1uyV17adGHxcXFj3Nych5wd+ASsqGhwWQ2WzzR0dGk0WgoNjaWrFZrFQ/hOJ4Rk/5Lcblc6tbW1keJiYkUExNDBoOB8vLyPFlZWZ7MzMw/UlNTHyQnJ69rtdoXefrNH4/ZbH6O5Ss2Rna7nUZHR2ljY4O2trZodXWVxsbGqK2tjYqKishkMh0xXpCWXl9sNtvL7MBJZWUlDQ4O0uTkJK2trdHe3p7A9vY2zc66aWRkhIaHh6mxsZFSU9O8HJXXpYR9agnKzc11p6WlUU1NDfX09FBXVxe1tLQQ5wQ1NTURGxBjDoeD6urqxHtvb68YwzqO2AdP7QSHs4d3f4qQQ3F1dTXOnTIyMpCAZ7BYLAIcJbLZMsQRyfNTUsxUUFCwKKn0X3hxHJfbTzhz7Ky0tJSVpVBSUtKFQHICWm0C6fV64nwR67hcKTs729PZ2VkiqfZP+vv7l7EYKCkpIZ1OJ7IfZYj2ImAsKiqKIiMjRZlyhYj1cIz17Uqqr5bl5eWw2dnZX/V6AyUkJAiEh4cLhIWF/atVAhwB4BkOxcfHi3nd3d2nR0dHdyQTvmV/f79uenpa7CQkJMQvBAcHCyjflW1hYSEdHh66JBO+5fj42I2aDg0NPVMMqNXqs1Z+Pv9+Wcu8QLu7u19IJnzLwcHBO2VlZSKUWHwRlDv1B8gR5ovHrP7q+2NnZ2cLiYczjIiIODt/+Rnne75POaYE+nCUyKOlpXs/M6VffW+sr6+/VVtbK2ocnM/8LhQgoeSkVL4r+/EsIy4uTlQAwo9ynpqaesjqr47A3Nzcq2A8JhAmFhsZjUZR26iKv1vf0OkAnXAeBMVsSuXl5cSJfV8y4VvcbnfZ0NCQWITFUAInnhQgrszMO4JHmpubaWZm5k3JhG/Z3Ny8zd5+X19fT8yGgnqhjK/bM8CA3BqNcvsPEHbcBfn5+VRVVUV9fX20uLj4kmTiallaWppyOrupoqJC0CrfbMIgwgpcFHoZGIfDYEJQOC6miYmJI78SUBaOgmZ8fPwb3HRwwGKxCuVISGWiKRMOwDPmIArp6eki/E6n07uwsFAtqfZf5ufny9n7EyQikgoGZK5H2V0GzIEjcBjODwwMrEgqn1w6OjpaOAlPQCRQjtqW+R4AW56HXP9YwxT8MYf+ep9ozAd23vXD8wZlNpQh92EenOBr+l3+ULmZ70NWqGW8zcp/v2zngCZUc8rjx3wdt/Oym/9s52OIY0MDbOQjxtf8/AO33zG+ZAfnOfR2nhawnxbsEMau8V+gUv0FJyWg+o5wz/8AAAAASUVORK5CYII=";
     private const string VALUE10 = "7df39ab6d5ea46e87b8a83870d05b202d946aa3999198ed543350c6ed211289e4fd1b289bc82190daf2ed41e1005b0bfaf98fc6e7d761d558129a35a529ff5b5";
+    private const string VALUE11 = "Test";
 
     private const string DEFAULTINIFILEPATH = @".\Sample\Sample.ini";
     private const string TMPDIR = @".\Sample\TMP";
@@ -131,9 +136,9 @@ public class Tests
     public void SampleIniHashMatchesPrecomputedHash()
     {
         byte[] iniBytes = File.ReadAllBytes(DEFAULTINIFILEPATH);
-        string resultHash = GenerateSHA512String(iniBytes);
+        string resultHash = GenerateSHA256String(iniBytes);
 
-        if (EXAMPLEINISHA512.Equals(resultHash))
+        if (EXAMPLEINISHA256.Equals(resultHash))
         {
             Assert.Pass();
         }
@@ -271,11 +276,28 @@ public class Tests
     }
 
     /// <summary>
-    /// Generates SHA512 hash represented as hex string (lowercase).
+    /// Test #9 - Check section and key case sensitivity.
     /// </summary>
-    private static string GenerateSHA512String(byte[] inputBytes)
+    [Test]
+    public void CheckSectionAndKeysCaseSensitivity()
     {
-        byte[] hashBytes = SHA512.HashData(inputBytes);
+        if (VALUE11.Equals(ini.GetValue(CASESENSITIVESECTION1, KEY11)) &&
+            VALUE11.Equals(ini.GetValue(CASESENSITIVESECTION1, KEY12)) &&
+            VALUE11.Equals(ini.GetValue(CASESENSITIVESECTION2, KEY11)) &&
+            VALUE11.Equals(ini.GetValue(CASESENSITIVESECTION2, KEY12)))
+        {
+            Assert.Pass();
+        }
+
+        Assert.Fail();
+    }
+
+    /// <summary>
+    /// Generates SHA256 hash represented as hex string (lowercase).
+    /// </summary>
+    private static string GenerateSHA256String(byte[] inputBytes)
+    {
+        byte[] hashBytes = SHA256.HashData(inputBytes);
         var sb = new StringBuilder();
 
         foreach (byte hashByte in hashBytes)
